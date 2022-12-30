8eff0f4fe4e9ca84ce4701fc9b3883d4c884527e
andreamlpython <avalletta@gmail.com>
2022-12-30 20:30:20 +0100

    Update plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -7,7 +7,8 @@ import seaborn as sns
 import torch.nn as nn
 
 from utils.data_transformers import view_as_image_plot_format, clip_image_to_zero_one
-from utils.profiling import treatment_profiles
+from utils.profiling import treatment_profiles, treatment_center_cells
+from models.LoadModels import LoadVAEmodel
 
 
 def img_saturate(img):
@@ -85,7 +86,7 @@ def plot_VAE_performance(plotdata, file=None, title=None):
     
     plt.close()
 
-def plot_cosine_similarity(x0, x1, model, file=None, title=None):
+def plot_cosine_similarity_old(x0, x1, model, file=None, title=None):
     #model could be eg. "model_dump/outputs_2022-12-04 - 12-20-15/"
     #x0.shape and x1.shape should be torch.Size([3, 68, 68])
     
@@ -131,7 +132,61 @@ def plot_cosine_similarity(x0, x1, model, file=None, title=None):
         plt.savefig(file)
     
     plt.close()
+
     
+def plot_cosine_similarity(Target, metadata_latent, model, model_type, file=None, title=None):
+    #model could be eg. "model_dump/outputs_2022-12-04 - 12-20-15/"
+    #x0.shape and x1.shape should be torch.Size([3, 68, 68])
+    
+    vae, validation_data, training_data, VAE_settings, vi = LoadVAEmodel(model, model_type)
+    '''vae = model
+    outputs0 = vae(x0[None,:,:,:])
+    outputs1 = vae(x1[None,:,:,:])
+
+    z0 = outputs0["z"].detach().numpy()
+    z1 = outputs1["z"].detach().numpy()'''
+    
+    tp = treatment_profiles(metadata_latent)
+    tcc = treatment_center_cells(metadata_latent,tp,p=2)
+    
+    z0 = tcc[tcc['Treatment'] == 'DMSO_0.0'].iloc[:,-256:].values
+    z1 = tcc[tcc['Treatment'] == Target].iloc[:,-256:].values
+
+    zs = np.linspace(z0, z1, num=10)
+
+    cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+
+    cp = []
+    for i in range(len(zs)):
+        input1 = vae.observation(torch.Tensor(zs[i]))[0]
+        benchmark = vae.observation(torch.Tensor(zs[9]))[0]
+        cp.append(cos(input1, benchmark).mean().detach().numpy().round(2))
+
+    # create figure
+    fig = plt.figure(figsize=(25, 10))
+
+    # setting values to rows and column variables
+    rows = 1
+    columns = len(zs)
+
+    # Adds a subplot at the 1st position
+    for i in range(columns):
+        fig.add_subplot(rows, columns, i+1)
+        plt.imshow((torch.permute(vae.observation(torch.Tensor(zs[i]))[0], (1, 2, 0))* 255).detach().numpy().astype(np.uint8))
+        plt.axis('off')
+        if i == 0:
+            plt.title('Control', y=-0.20, fontsize=26)
+        elif i == len(zs)-1:
+            plt.title('Target', y=-0.20, fontsize=26)
+        else:
+            plt.title(cp[i], fontsize=26)
+    if file == None:
+        plt.show()
+    else: 
+        plt.savefig(file)
+    
+    plt.close()
+ 
     
 def heatmap(metadata_latent):
     metadata_onehot = pd.concat([metadata_latent, pd.get_dummies(metadata_latent["moa"], prefix = 'onehot_moa')], axis=1)
